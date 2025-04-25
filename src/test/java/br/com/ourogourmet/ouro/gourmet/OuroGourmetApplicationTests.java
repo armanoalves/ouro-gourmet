e5d3c9fbc34ec880a74288632b7f01f547040d34
armanoalves <armanobarrosalvesh@gmail.com>
2025-04-24 21:09:35 -0300

    first commit

---
+++ src/test/java/br/com/ourogourmet/ouro/gourmet/OuroGourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ourogourmet.ouro.gourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuroGourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
