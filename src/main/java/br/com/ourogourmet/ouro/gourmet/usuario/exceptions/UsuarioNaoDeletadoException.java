a895477958f401a118c67b362d64d29cf961cb22
LeoBlanco123 <rm99119@fiap.com.br>
2025-05-26 14:20:38 -0300

    teste

--- src/main/java/br/com/ourogourmet/ouro/gourmet/usuario/exceptions/UsuarioNaoDeletadoException.java
+++ src/main/java/br/com/ourogourmet/ouro/gourmet/usuario/exceptions/UsuarioNaoDeletadoException.java
@@ -4,4 +4,5 @@ public class UsuarioNaoDeletadoException extends RuntimeException {
     public UsuarioNaoDeletadoException(String id) {
         super("Não foi possível deletar o usuário. ID: " + id);
     }
+
 }
